16422c2ae3f8523ef00f555eca3ccba616836c96
Jaume Serrats <jaume.serrats@gmail.com>
2018-09-26 10:31:46 +0200

    small improvements

--- soft/basicjscrypto/basic_crypto/aes_demo.js
+++ soft/basicjscrypto/basic_crypto/aes_demo.js
@@ -20,8 +20,6 @@ function decrypt(text) {
 
 message = "01234567890123456789012345678901234567890123456";
 var hw = encrypt(message);
-split = hw.toString().length / 2;
-console.log("Message is " + message.length + " bytes long. " +
-    "Encrypted is " + hw.toString().length + " chars or " + hw.length /2 + "bytes");
-console.log(hw, "\n" + hw.substr(0, split), "\n" + hw.substr(split));
-console.log(decrypt(hw));
+console.log('Original message: ' + message);
+console.log('Encrypted message: ' + hw.toString());
+console.log('Decrypted message: ' + decrypt(hw));
--- soft/basicjscrypto/basic_crypto/rsa_demo.js
+++ soft/basicjscrypto/basic_crypto/rsa_demo.js
@@ -1,5 +1,3 @@
-// openssl genrsa -out certs/server/my-server.key.pem 2048
-// openssl rsa -in certs/source/my-source.key.pem -pubout -out certs/destination/my-source.pub
 var fs = require('fs');
 const NodeRSA = require('node-rsa');
 
@@ -7,10 +5,15 @@ private_key = new NodeRSA(fs.readFileSync('key.priv'));
 public_key = new NodeRSA(fs.readFileSync('key.pub'));
 
 
-data = "testaedaedaedae";
+data = "testing asymmetric encryption";
 
-console.log(private_key.decrypt(public_key.encrypt(data, 'base64'), 'utf8'));
+
+encrypted_data = public_key.encrypt(data, 'base64');
+console.log('Encryoted data: ' + encrypted_data);
+console.log('Decrypted data: ' + private_key.decrypt(encrypted_data, 'utf8'));
 
 signed = private_key.sign(data, 'base64');
 
-console.log(public_key.verify(data, signed, 'utf8', 'base64'));
+if (public_key.verify(data, signed, 'utf8', 'base64')){
+    console.log('The data was signed with our key');
+}
